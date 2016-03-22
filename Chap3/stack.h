056b8070d0a91725a475f90e60414a32aa96acba
Cpcong <593029121@qq.com>
2016-03-21 19:44:46 -0700

    add stack code

---
+++ Chap3/stack.h
@@ -0,0 +1,15 @@
+#ifndef _STACK_H
+#define _STACK_H
+
+struct Node;
+typedef struct Node *PtrToNode;
+typedef PtrToNode Stack;
+typedef int ElementType;
+
+int IsEmpty(Stack s);
+Stack CreateStack();
+void MakeEmpty(Stack s);
+void Push(ElementType x, Stack s);
+ElementType Top(Stack s);
+void Pop(Stack s);
+#endif
