9ba5058e3b49f4b0bbfb7042f825ffb26d04fe90
pcer <pengcong.chen@outlook.com>
2016-03-24 19:05:26 -0700

    add shellsortcode by hibbard increment

--- Chap7/shellSort.c
+++ Chap7/shellSort.c
@@ -1,5 +1,9 @@
 #include "stdio.h"
+#include "math.h"
+#include "stdlib.h"
 
+//shell increment(1, 2, 4, ..)
+/*
 void ShellSort(int a[], int n)
 {
     int increment;
@@ -20,12 +24,63 @@ void ShellSort(int a[], int n)
     }    
     
 }
+*/
+
+static int InitIncSeqLen(n)
+{
+    int i = 1;;
+    int increment = 2;
+    while(increment - 1 < sqrt((double)n)){
+        i++;
+        increment *= 2;
+    }
+    return i - 1;
+}
+static void SetIncrementArr(int a[], int n)
+{
+    int i;
+    int increment = 1;
+    for(i = 0;i < n;i++){
+        increment *= 2;
+        a[i] = increment - 1;
+    } 
+}
+//Hibbard increment(1, 3, 7.... 2^k -1)
+//limit: increment < sqrt(n)
+void ShellSort(int a[], int n)
+{
+    int increment;
+    int len;
+    int i, j;
+    int t;
+    int *incrementArr;
+    len = InitIncSeqLen(n);
+    incrementArr = malloc(sizeof(int) * len);
+    SetIncrementArr(incrementArr, len); 
+    for(len = len -1;len >= 0;len--){
+        increment = incrementArr[len];
+        for(i = increment;i < n;i++){
+            t = a[i]; 
+            for(j = i;j >= increment;j -= increment){
+                if(t < a[j - increment]){
+                    a[j] = a[j - increment]; 
+                }else{
+                    break;
+                }
+            }
+            a[j] = t;
+        }
+    }
+}
 
 int main()
 {
-    int a[] = {2, 4, 5, 3, 6, 1, 8, 9, 13, 11, 12};
-    int len = sizeof(a)/sizeof(a[0]);
     int i;
+    int a[100];
+    int len = sizeof(a)/sizeof(a[0]);
+    for(i = 0;i < 100;i++){
+        a[i] = rand()%100;
+    }
     ShellSort(a, len);
     for(i = 0;i < len;i++){
         printf("%d ", a[i]);
